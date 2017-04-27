AOSPA/android_vendor_pa

pa: prebuilt: Add hostapd configuration
* hostapd configuration is not platform-specific, so it is safe to
  include it globally.
* This addresses lack of hostapd config on nexus devices since Google
  handles hostapd is a different way.

Change-Id: Ibb100b7eae1a170761aa130652ce4972dbe794c7
Signed-off-by: Evan Anderson <evananderson@aospa.co>

main.mk
@@ -84,6 +84,12 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/pa/prebuilt/addon.d/50-backuptool.sh:system/addon.d/50-backuptool.sh
 
+# Include hostapd configuration
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/etc/hostapd/hostapd.conf:system/etc/hostapd/hostapd.conf \
+    vendor/pa/prebuilt/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
+    vendor/pa/prebuilt/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
+
 # Build Chromium for Snapdragon (PA Browser)
 PRODUCT_PACKAGES += PABrowser
 

pa-a6000/vendor_pa

main.mk
@@ -88,6 +88,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/install/gapps-config.txt:install/gapps-config.txt
 
+# Include hostapd configuration
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    vendor/pa/prebuilt/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
+    vendor/pa/prebuilt/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
+
 # Build Snapdragon apps
 PRODUCT_PACKAGES += \
     SnapdragonGallery \